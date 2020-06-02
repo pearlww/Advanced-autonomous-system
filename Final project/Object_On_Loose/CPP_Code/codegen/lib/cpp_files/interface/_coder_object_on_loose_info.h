65d1808d0129241ae16cfca4b4436100d9222908
Jeremy Coffelt <47064066+GearsAmperes@users.noreply.github.com>
2020-06-02 15:09:18 +0200

    Add files via upload

---
+++ Final project/Object_On_Loose/CPP_Code/codegen/lib/cpp_files/interface/_coder_object_on_loose_info.h
@@ -0,0 +1,26 @@
+/*
+ * Student License - for use by students to meet course requirements and
+ * perform academic research at degree granting institutions only.  Not
+ * for government, commercial, or other organizational use.
+ * File: _coder_object_on_loose_info.h
+ *
+ * MATLAB Coder version            : 4.3
+ * C/C++ source code generated on  : 29-May-2020 15:35:37
+ */
+
+#ifndef _CODER_OBJECT_ON_LOOSE_INFO_H
+#define _CODER_OBJECT_ON_LOOSE_INFO_H
+
+/* Include Files */
+#include "mex.h"
+
+/* Function Declarations */
+MEXFUNCTION_LINKAGE mxArray *emlrtMexFcnProperties(void);
+
+#endif
+
+/*
+ * File trailer for _coder_object_on_loose_info.h
+ *
+ * [EOF]
+ */
